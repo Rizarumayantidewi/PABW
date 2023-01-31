8a9a1891c1d412bf9d5e9cbb96eceaabb77ebc1b
Riza Rumayanti Dewi <79615980+Rizarumayantidewi@users.noreply.github.com>
2023-02-01 05:52:26 +0700

    Create Persegi.php

---
+++ Sesi 11/multipart-formdata/Persegi.php
@@ -0,0 +1,29 @@
+<?php 
+ class Persegi {
+    public $sisi;
+    public $luas;
+
+    public function __construct($sisi) {
+        $this->setSisi($sisi);
+    }
+
+    private function hitungLuas() {
+        $this->luas = $this->sisi * $this->sisi;
+    }
+    
+    private function setSisi($sisi) {
+        $this->sisi = $sisi;
+        $this->hitungLuas();
+    }
+
+    public function getSisi() {
+        return $this->sisi;
+    }
+
+    public function getLuas() {
+        return $this->luas;
+    }
+}
+
+$persegi = new Persegi(10);
+echo $segitiga->getLuas();
